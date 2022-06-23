0d3fcad10762fe2fb941ac36b0d7dcf687d489d9
sarthak <sarthakbatham26@gmail.com>
2022-06-23 16:49:20 +0530

    styling changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import "./App.css";
 import Navbar from "./Components/navbar/Navbar";
 import { ModeContext } from "./Context";
 import { useState } from "react";
+import Week from "./Components/mode/Week";
+import DayOfWeek from "./Components/mode/days/DayOfWeek";
+import Day from "./Components/mode/days/Day";
 
 function App() {
   const ModeState = useState("day");
+
+  function displayLayout() {
+    switch (ModeState[0]) {
+      case "day":
+        return <Day date={new Date()} events={[]} />;
+      case "week":
+        return <Week date={new Date()} />;
+      case "month":
+      case "year":
+      default:
+        return;
+    }
+  }
 
   return (
     <div className="App">
@@ -12,6 +28,7 @@ function App() {
         <header className="App-header">
           <Navbar />
         </header>
+        <main>{displayLayout()}</main>
       </ModeContext.Provider>
     </div>
   );
