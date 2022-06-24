24111b67fae6a921bbdf5a4962e03578832292a8
sarthak <sarthakbatham26@gmail.com>
2022-06-24 23:50:47 +0530

    add new class to date cell

--- src/Components/mode/cells/Cell.js
+++ src/Components/mode/cells/Cell.js
@@ -1,5 +1,9 @@
 function Cell(props) {
-  return <div className={`cell ${props.className}`}>{props.children}</div>;
+  return (
+    <div className={`cell ${props?.className || "date-cell"}`}>
+      {props.children}
+    </div>
+  );
 }
 
 export default Cell;
